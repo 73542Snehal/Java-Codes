ef1de460c09a642309b3b584bb58466a46a9dc8c
HP <HP@Snehal>
2023-07-19 17:56:22 +0530

    c

---
+++ exceptionHandling_23/src/exceptionHandling/DivideByZeroException.java
@@ -0,0 +1,5 @@
+package exceptionHandling;
+//click on new class>>give name >> and in superclass instead of java.lang.Object type Exception >> finish
+public class DivideByZeroException extends Exception {
+
+}
